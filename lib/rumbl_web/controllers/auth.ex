d0e53d253528ff8e9a928ea446d4fc1cb8db0009
eltonplima <eltonplima@gmail.com>
2019-05-31 05:55:38 -0300

    Add basic login

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -12,4 +12,10 @@ defmodule RumblWeb.Auth do
     assign(conn, :current_user, user)
   end
 
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
 end
